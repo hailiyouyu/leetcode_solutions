e943759b1a2ade85fe180b0d62cc9575894c5f7e
hailiyouyu <hailiyouyu.hs@gmail.com>
2015-09-10 05:57:46 -0700

    94 Binary Tree Inorder Traversal

---
+++ binary_tree_inorder_traversal.cc
@@ -0,0 +1,19 @@
+class Solution {
+    vector<int> inorderTraversal(TreeNode *root) {
+	vector<int> data;
+	stack<TreeNode *> nodes;
+
+	while (!root || !nodes.empty()) {
+	    if (root) {
+		nodes.push(root);
+		root = root->left;
+	    } else {
+		TreeNode *node = nodes.top();
+		nodes.pop();
+		data.push_back(node->val);
+		root = node->right;
+	    }
+	}
+	return data;
+    }
+};
