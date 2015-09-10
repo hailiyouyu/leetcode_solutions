566b490299596ff1c8bea0bd0e39ffc56eab73dd
hailiyouyu <hailiyouyu.hs@gmail.com>
2015-09-10 05:15:04 -0700

    144 Binary Preorder Traversal

---
+++ binary_tree_preorder_traversal.cc
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode *root) {
+	vector<int> data;
+	if (!root)
+	    return data;
+	stack<int> nodes;
+	nodes.push(root);
+
+	while (!nodes.empty()) {
+	    TreeNode *pNode = nodes.top();
+	    nodes.pop();
+
+	    data.push_back(pNode->val);
+
+	    if (pNode->right)
+		nodes.push(pNode->right);
+	    if (pNode->left)
+		nodes.push(pNode->left);
+	}
+	return data;
+    }
+};
