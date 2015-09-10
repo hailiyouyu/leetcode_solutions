c3bd6b74fd044980c8afc01d3e20f22414a77056
hailiyouyu <hailiyouyu.hs@gmail.com>
2015-09-10 06:16:39 -0700

    145 Binary Tree Postorder Traversal

---
+++ binary_tree_postorder_traversal.cc
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode *root) {
+	vector<int> data;
+	if (!root)
+	    return data;
+
+	stack<TreeNode *> tempNodes;
+	stack<TreeNode *> nodes;
+	TreeNode *pNode;
+
+	tempNodes.push(root);
+	while (!tempNodes.empty()) {
+	    pNode = tempNodes.top();
+	    tempNodes.pop();
+
+	    nodes.push(pNode);
+	    if (pNode->left)
+		tempNodes.push(pNode->left);
+	    if (pNode->right)
+		tempNodes.push(pNode->right);
+	}
+	while (!nodes.empty()) {
+	    pNode = nodes.top();
+	    nodes.pop();
+	    data.push_back(pNode->val);
+	}
+	return pNode;
+    }
+};
