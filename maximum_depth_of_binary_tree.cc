d50faeb74b7b31e30da0ac9e22247be7bc359fbe
hailiyouyu <hailiyouyu.hs@gmail.com>
2015-09-11 00:00:37 +0800

    104 Maximum Depth of Binary Tree

---
+++ maximum_depth_of_binary_tree.cc
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        if (!root)
+            return -1;
+        else
+            return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
