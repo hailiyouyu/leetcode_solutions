b7f8cc0350713cff526ccf5edcb512d9db19eb5c
hailiyouyu <hailiyouyu.hs@gmail.com>
2015-09-11 00:55:28 -0700

    111 Minimum Depth Of Binary Tree

---
+++ minimum_depth_of_binary_tree.cc
@@ -0,0 +1,30 @@
+class Solution {
+    int minDepth(TreeNode *root) {
+        if (!root)
+            return 0;
+        stack<pair<TreeNode *, bool>> nodes;
+        int depth = 0;
+        
+        while (root || !nodes.empty()) {
+            if (root) {
+                nodes.push(make_pair(root, true));
+                root = root->left;
+            } else {
+                pair<TreeNode *, bool> node = nodes.top();
+                nodes.pop();
+
+                if (node.second) {
+                    node.second = false;
+                    nodes.push(node);
+                    root = node.first->right;
+                } else {
+                    if (!node.first->left && !node.first->second) {
+                        int tempDepth = nodes.size() + 1;
+                        depth = min(depth, tempDepth);
+                    }
+                    root = nullptr;
+                }
+            }
+        }
+    }
+};
