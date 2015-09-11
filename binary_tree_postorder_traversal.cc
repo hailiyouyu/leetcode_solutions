0be8bc75f7aac214319bef1e025cfe719f8aa6f4
hailiyouyu <hailiyouyu.hs@gmail.com>
2015-09-11 00:38:17 -0700

    solution 2

--- binary_tree_postorder_traversal.cc
+++ binary_tree_postorder_traversal.cc
@@ -28,3 +28,30 @@ public:
 	return data;
     }
 };
+
+class Solution2 {
+public:
+    vector<int> postorderTraversal(TreeNode *root) {
+        vector<int> data;
+        stack<pair<TreeNode *, bool>> nodes;
+
+        while (root || !nodes.empty()) {
+            if (root) {
+                nodes.push(make_pair(root, true));
+                root = root->left;
+            } else {
+                pair<TreeNode *, bool> node = nodes.top();
+                nodes.pop();
+
+                if (node.second) {
+                    node.second = false;
+                    nodes.push(node);
+                    root = node.left->right;
+                } else {
+                    data.push_back(node.left->val);
+                    root = nullptr;
+                }
+            }
+        }
+    }
+};
--- maximum_depth_of_binary_tree.cc
+++ maximum_depth_of_binary_tree.cc
@@ -7,3 +7,34 @@ public:
             return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
     }
 };
+
+/* non-recursively */
+class Solution2 {
+public:
+    int maxDepth(TreeNode *root) {
+        if (!root)
+            return 0;
+        stack<pair<TreeNode *, bool>> nodes;
+        int depth = 0;
+
+        while (root || !nodes.empty()) {
+            if (root) {
+                nodes.push(make_pair(root, true));
+                root = root->left;
+            } else {
+                pair<TreeNode *, bool> node = nodes.top();
+                nodes.pop();
+
+                if (node.second) {
+                    node.second = false;
+                    nodes.push(node);
+                    root = node.first->right;
+                } else {
+                    if (!node.first->left && !node.first->right)
+                        depth = max(depth, nodes.size()+1);
+                    root = nullptr;
+                }
+            }
+        }
+    }
+};
