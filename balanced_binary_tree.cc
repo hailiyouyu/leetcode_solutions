0d35ecc7d2dbcf7d46d8fb3353d76c530f81336e
hailiyouyu <hailiyouyu.hs@gmail.com>
2015-09-14 00:45:31 -0700

    110 Balanced Binary Tree

--- balanced_binary_tree.cc
+++ balanced_binary_tree.cc
@@ -19,3 +19,27 @@ public:
             return max(height(root->left), height(root->right)) + 1;
     }
 };
+
+class Solution {
+public:
+    bool isBalanced(TreeNode *root) {
+        int depth = 0;
+        return isBalanced(root, depth);
+    }
+    bool isBalanced(TreeNode *root, int &depth) {
+        if (!root) {
+            depth = 0;
+            return true;
+        }
+        int left_depth = 0;
+        int right_depth = 0;
+        if (!isBalanced(root->left, left_depth) || !isBalanced(root->right, right_depth))
+            return false;
+        
+        int diff = left_depth - right_depth;
+        if (diff < -1 && diff > 1)
+            return false;
+        depth = max(left_depth, right_depth) + 1;
+        return true;
+    }
+};
