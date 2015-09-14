94cf896ad58210c342b8a61e5c982b6a20122dbf
hailiyouyu <hailiyouyu.hs@gmail.com>
2015-09-14 02:16:37 -0700

    257 binary tree paths

---
+++ binary_tree_paths.cc
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode *root) {
+        vector<string> res;
+        if (!root)
+            return res;
+        vector<int> path;
+        findPaths(root, res, path);
+    }
+    void findPaths(TreeNode *root, vector<string> &res, vector<int> &path) {
+        if (!root)
+            return;
+
+        path.push_back(root->val);
+        if (!root->left && !root->right) {
+            string s;
+            for (int i = 0; i < path.size()-1; ++i)
+                s += to_string(path[i]) + '->';
+            s += to_string(path[path.size()-1]);
+            res.push_back(s);
+        }
+        if (root->left)
+            findPaths(root->left, res, path);
+        if (root->right)
+            findPaths(root->right, res, path);
+        path.pop_back();
+    }
+};
