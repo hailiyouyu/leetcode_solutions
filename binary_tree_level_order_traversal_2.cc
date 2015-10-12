07aaa36bc061f589c04f10057dc0a2de1daf647f
hailiyouyu <hailiyouyu.hs@gmail.com>
2015-10-12 18:42:46 +0800

    107 binary tree level order traversal 2

--- binary_tree_level_order_traversal_2.cc
+++ binary_tree_level_order_traversal_2.cc
@@ -1,11 +1,12 @@
 class Solution {
-    public vector<vector<int>> levelOrderBottom(TreeNode *root) {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode *root) {
         vector<vector<int>> res;
         if (!root)
             return res;
         queue<TreeNode *> nodes;
         nodes.push(root);
-        while (!nodes.emtpy()) {
+        while (!nodes.empty()) {
             int n = nodes.size();
             vector<int> level;
             for (int i = 0; i < n; ++i) {
@@ -19,9 +20,9 @@ class Solution {
                     nodes.push(node->right);
                 }
             }
-            res.push(level);
+            res.push_back(level);
         }
+        reverse(res.begin(), res.end());
+        return res;
     }
-    reverse(res.begin(), res.end());
-    return res;
 };
