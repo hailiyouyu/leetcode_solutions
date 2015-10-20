0fa59c8cf01c4a5a9f16db6f32825e5c65dd7886
hailiyouyu <hailiyouyu.hs@gmail.com>
2015-10-20 17:13:10 +0800

    129 sum root to leaf numbers

---
+++ sum_root_to_leaf_numbers.cc
@@ -0,0 +1,40 @@
+class Solution {
+public:
+  int sumNumbers(TreeNode *root) {
+    vector<int> sum;
+    findPath(root, sum);
+    return accumulate(sum.begin(), sum.end(), 0);
+  }
+
+  void findPath(TreeNode *root, vector<int> &sum) {
+    stack<pair<TreeNode *, bool>> nodes;
+    vector<int> path;
+
+    while (root || !nodes.empty()) {
+      if (root) {
+        nodes.push(make_pair(root, true));
+        path.push_back(root->val);
+        root = root->left;
+      } else {
+        pair<TreeNode *, bool> node = nodes.top();
+        nodes.pop();
+
+        if (node.second) {
+          node.second = false;
+          nodes.push(node);
+          root = node.first->right;
+        } else {
+          if (!node.first->left && !node.first->right) {
+            int s = 0;
+            for (int i = 0; i != path.size(); ++i) {
+              s += 10 * s + path[i];
+            }
+            sum.push_back(s);
+          }
+          path.pop_back();
+          root = NULL;
+        }
+      }
+    }
+  }
+};
