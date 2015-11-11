8aa2912f78c6a6ad2efce93e7e20f748f9be45a9
hailiyouyu <hailiyouyu.hs@gmail.com>
2015-11-11 21:04:22 +0800

    283 move zeros

--- move_zeros.cc
+++ move_zeros.cc
@@ -5,7 +5,7 @@ public:
   void moveZeros(vector<int>& nums) {
     int i = 0;
     for (auto j = 0; j < nums.size(); ++j) {
-      if (nums[j] > 0) {
+      if (nums[j] != 0) { // can't write nums[j] > 0, maybe there are negatives
         using std::swap;
         swap(nums[i], nums[j]);
         ++i;
