af0f46751726b7bfa0582faa8df75ed9b467b16c
hailiyouyu <hailiyouyu.hs@gmail.com>
2015-11-11 20:15:08 +0800

    292 nim game

--- nim_game.cc
+++ nim_game.cc
@@ -1,24 +1,24 @@
 class solution {
 public:
   bool canWinNim(int n) {
-    if (n == 1 || n == 3)
-      return true;
-    if (n == 0 || n == 2 || n == 4)
+    if (n == 0)
       return false;
-    return !canWinNim(n-1) || !canWinNim(n-3);
+    if (n <= 3)
+      return true;
+    return !canWinNim(n-1) || !canWinNim(n-2) || !canWinNim(n-3);
   }
 };
 
 class Solution2 {
 public:
   bool canWinNim(int n) {
-    if (n == 1 || n == 3)
-      return true;
-    if (n == 0 || n == 2)
+    if (n == 0)
       return false;
-    vector<bool> base = {false, true, false}; // 1, 2, 3
+    if (n <= 3)
+      return true;
+    vector<bool> base = {true, true, true}; // 1, 2, 3
     for (auto i = 4; i <= n; ++i) {
-      bool t = !base[i-1] || !base[i-3];
+      bool t = !base[i-1] || !base[i-2] || !base[i-3];
       base[0] = base[1];
       base[1] = base[2];
       base[2] = t;
