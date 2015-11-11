47d52e2e0436b1e5ba6a991e473db76340dc77e6
hailiyouyu <hailiyouyu.hs@gmail.com>
2015-11-11 20:29:43 +0800

    292 nim game

--- nim_game.cc
+++ nim_game.cc
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 class solution {
 public:
   bool canWinNim(int n) {
@@ -11,14 +14,14 @@ public:
 
 class Solution2 {
 public:
-  bool canWinNim(int n) {
+  static bool canWinNim(int n) {
     if (n == 0)
       return false;
-    if (n <= 3)
+    if (n < 3)
       return true;
-    vector<bool> base = {true, true, true}; // 1, 2, 3
-    for (auto i = 4; i <= n; ++i) {
-      bool t = !base[i-1] || !base[i-2] || !base[i-3];
+    vector<bool> base = {false, true, true}; // 1, 2, 3
+    for (auto i = 3; i <= n; ++i) {
+      bool t = !base[0] || !base[1] || !base[2];
       base[0] = base[1];
       base[1] = base[2];
       base[2] = t;
