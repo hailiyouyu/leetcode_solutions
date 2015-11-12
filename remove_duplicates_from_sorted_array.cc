4657a5c8289d578a6f873612fc41a0e934e427b9
hailiyouyu <hailiyouyu.hs@gmail.com>
2015-11-12 22:44:43 +0800

    26 remove duplicates form sorted array

--- remove_duplicates_from_sorted_array.cc
+++ remove_duplicates_from_sorted_array.cc
@@ -2,8 +2,12 @@ class Solution {
 public:
   int removeDuplicates(vector<int>& nums) {
     int i = 0;
-    for (int j = 0; j != nums.size(); ++j) {
-      if ()
+    for (int j = 1; j < nums.size(); ++j) {
+      if (nums[i] != nums[j]) {
+        swap(nums[i+1], nums[j]);
+        ++i;
+      }
     }
+    return i;
   }
 };
