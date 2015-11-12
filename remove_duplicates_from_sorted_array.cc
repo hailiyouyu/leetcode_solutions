1d62a40d46340f9419b40af094a4f86870e71a14
hailiyouyu <hailiyouyu.hs@gmail.com>
2015-11-12 22:48:07 +0800

    26 remove duplicates form sorted array

--- remove_duplicates_from_sorted_array.cc
+++ remove_duplicates_from_sorted_array.cc
@@ -1,6 +1,8 @@
 class Solution {
 public:
   int removeDuplicates(vector<int>& nums) {
+    if (nums.empty())
+      return 0;
     int i = 0;
     for (int j = 1; j < nums.size(); ++j) {
       if (nums[i] != nums[j]) {
@@ -8,6 +10,6 @@ public:
         ++i;
       }
     }
-    return i;
+    return i+1;
   }
 };
